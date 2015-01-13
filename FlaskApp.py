3ae7469275d5d156c8c6334433cab34ef35f850e
blighli <blighli@gmail.com>
2015-01-14 02:07:42 +0800

    run in debug mode and auto reload

--- FlaskApp.py
+++ FlaskApp.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Good! Hello World!'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
