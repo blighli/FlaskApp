a7b6176b928bf6aedf3782190ee0cb89342f7473
blighli <blighli@gmail.com>
2015-01-18 04:22:32 +0800

    add admin layouts

--- FlaskApp.py
+++ FlaskApp.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/test')
 def test():
     return render_template('ReadMe.txt')
