f168e0a4c16c35ee3bd445946752e108933d1a10
blighli <blighli@gmail.com>
2015-01-19 03:56:55 +0800

    使用jQuery控制菜单项中当前页面的标记

--- FlaskApp.py
+++ FlaskApp.py
@@ -5,27 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/admin.html')
 def admin():
     items = range(200)
     return render_template('admin.html',items=items)
 
-@app.route('/ace_base.html')
-def base():
-    return render_template('ace_base.html')
-
 @app.route('/login.html')
 def login():
     return render_template('login.html')
-
-@app.route('/test.html')
-def test():
-    return render_template('ReadMe.txt')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
